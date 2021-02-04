b31dcd3a036dd185ea03385ec4f0088ae0fa818f
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-02-04 20:26:32 +0300

    feat: remove useless code

--- index.js
+++ index.js
@@ -43,42 +43,6 @@ function onPageLoaded() {
             let parentNode = el.parentNode;
 
             el.addEventListener('keydown', event => eventListener(el, event));
-                // (event) => {
-                /*let parentNode = el.parentNode;
-                let elValue = event.target.value;
-                const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g;
-                const passwordRegex = '';
-
-                if (elValue !== '') {
-                    if ((el === firstName || el === lastName) && elValue.trim() !== '') {
-                        if  (parentNode.querySelector('.notification')) {
-                            parentNode.querySelector('.notification').remove();
-                        }
-                        el.classList.remove(warnClass);
-                        el.classList.add(acceptClass)
-                    } else if (el === email && emailRegex.test(elValue)) {
-                        console.log(elValue);
-                    }
-                }*/
-                // if (el === email) {}
-                // if (el === password) {}
-                // if (el === confirmPassword) {}
-            // })
-
-            // el.addEventListener('focus', (event) => {
-            //     let parentNode = el.parentNode;
-            //     let elValue = event.target.value;
-            //
-            //     if (elValue.trim() === '' && !parentNode.querySelector('.notification')) {
-            //         addWarnMessage(parentNode);
-            //         el.classList.add(warnClass);
-            //         el.classList.remove(acceptClass);
-            //     } else if (elValue.trim() !== '' && parentNode.querySelector('.notification')) {
-            //         el.classList.remove(warnClass);
-            //         parentNode.querySelector('.notification').remove();
-            //         el.classList.add(acceptClass);
-            //     }
-            // });
         }
     }
 
